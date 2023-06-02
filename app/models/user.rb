ffc04e4043a906752d5d1e0f0d433863835699c6
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-02 15:43:45 +0000

    bookers2-debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
+  
+  has_many :favorites, dependent: :destroy
 
 
 
