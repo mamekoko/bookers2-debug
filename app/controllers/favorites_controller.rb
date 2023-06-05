f3bce4adce1cfc3d1d03664632b43cd1170a8d5a
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-05 15:25:40 +0000

    bookers2-debug

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+  
+  def create
+    books = Book.find(params[:books_id])
+    favorite = current_user.favorites.new(books_id: books.id)
+    favorite.save
+    redirect_to books_path
+  end
+
+  def destroy
+    books = Book.find(params[:books_id])
+    favorite = current_user.favorites.find_by(books_id: books.id)
+    favorite.destroy
+    redirect_to books_path
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2023_06_02_150654) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "post_image_id"
+    t.integer "books_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
