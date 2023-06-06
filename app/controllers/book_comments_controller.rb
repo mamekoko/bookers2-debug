d40d7d88c1b0ef0c613fba73a448bbb14acc5848
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-06 13:15:16 +0000

    bookers2-debug

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,2 +1,17 @@
 class BookCommentsController < ApplicationController
+  
+  def create
+    book = Book.find(params[:books_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.books_id = books.id
+    comment.save
+    redirect_to books_path(books)
+  end
+
+  private
+
+  def post_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+  
 end
