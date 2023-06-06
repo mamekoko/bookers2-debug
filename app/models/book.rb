801e23dc2d7b0dafae3c8c2cde1d036ebafb2128
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-06 13:41:46 +0000

    bookers2-debug

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,16 +1,17 @@
 class Book < ApplicationRecord
   belongs_to :user
-  validates :title,presence:true
-  validates :body,presence:true,length:{maximum:200}
 
   has_many :favorites, dependent: :destroy
 
   has_many :book_comments, dependent: :destroy
 
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+
 
 
   def favorited_by?(user)
-    favorites.exists?(user_id: user.id)
+    favorites.where(user_id: user.id).exists?
   end
 
 end
