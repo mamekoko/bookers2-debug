d7bd8d6e2d539fd8394a0ebde1da3d9e4e654f6a
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-06 14:15:11 +0000

    bookers2-debug

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy,]
-  
+
   resources :users, only: [:index, :show, :edit, :update] do
+  resources :book_comments, only: [:create, :destroy]
   resource :favorites, only: [:create, :destroy]
-  resources :book_comments, only: [:create]
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
