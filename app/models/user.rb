92dea59ae9a54816b87a4fbe1d175b34eca13e78
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-08 09:10:58 +0000

    bookers2-debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
 
   has_many :book_comments, dependent: :destroy
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
 
   has_one_attached :profile_image
 
