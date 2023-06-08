dbf9d97c102c1e1fcd6a4b519a129213a8bb3b8f
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-08 07:19:56 +0000

    bookers2-debug

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about", as: 'about'
   devise_for :users
 
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy,]
-
-  resources :users, only: [:index, :show, :edit, :update] do
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy,] do
   resources :book_comments, only: [:create, :destroy]
   resource :favorites, only: [:create, :destroy]
   end
+
+  resources :users, only: [:index, :show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
