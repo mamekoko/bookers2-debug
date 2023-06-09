9008aa4bfab727dad98dee2300963519633fb08f
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-09 14:33:06 +0000

    bookers2-debug

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about"=>"homes#about", as: 'about'
   devise_for :users
+  
+  get "search" => "searches#search"
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy,]
   resources :users, only: [:index, :show, :edit, :update] do
