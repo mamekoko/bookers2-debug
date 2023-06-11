5ec0029fad83ff93bb3a203e2208e97c26b979be
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-11 16:28:06 +0000

    bookers2-debug

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,14 @@ Rails.application.routes.draw do
   devise_for :users
 
 
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy,]
-  resources :users, only: [:index, :show, :edit, :update] do
-  resources :book_comments, only: [:create, :destroy]
-  resource :favorites, only: [:create, :destroy]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
   end
 
   get '/search', to: 'searches#search'
